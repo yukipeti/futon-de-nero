8bdcbf153b9d2f58d07826d2e3c70d01e47f78a0
yukipeti <ugachan0817@gmail.com>
2024-04-22 14:34:00 +0900

    add futon-de-nero command

--- index.js
+++ index.js
@@ -1,40 +1,31 @@
-const fs = require("node:fs");
-const path = require("node:path");
-const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
-const { token } = require("./config.json");
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.commands = new Collection();
 
 // commandsフォルダから、.jsで終わるファイルのみを取得
-const commandsPath = path.join(__dirname, "commands");
-const commandFiles = fs
-    .readdirSync(commandsPath)
-    .filter((file) => file.endsWith(".js"));
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
-    const commands = require(filePath);
-
-    for (const command of commands) {
-        //console.log(command);
-        if (command.data && command.execute) {
-            client.commands.set(command.data.name, command);
-        } else {
-            console.error(
-                `[WARNING]  ${filePath} のコマンドには、必要な "data" または "execute" プロパティがありません。`,
-            );
-        }
+    const command = require(filePath);
+    if ('data' in command && 'execute' in command) {
+        client.commands.set(command.data.name, command);
+    } else {
+        console.log(`[WARNING]  ${filePath} のコマンドには、必要な "data" または "execute" プロパティがありません。`);
     }
 }
 
 // コマンドが送られてきた際の処理
-client.on(Events.InteractionCreate, async (interaction) => {
+client.on(Events.InteractionCreate, async interaction => {
     // コマンドでなかった場合は処理せずさよなら。
     if (!interaction.isChatInputCommand()) return;
 
     const command = interaction.client.commands.get(interaction.commandName);
-    console.log(command);
 
     // 一致するコマンドがなかった場合
     if (!command) {
@@ -47,13 +38,11 @@ client.on(Events.InteractionCreate, async (interaction) => {
         await command.execute(interaction);
     } catch (error) {
         console.error(error);
-        await interaction.reply({
-            content: "コマンドを実行中にエラーが発生しました。",
-            ephemeral: true,
-        });
+        await interaction.reply({ content: 'コマンドを実行中にエラーが発生しました。', ephemeral: true });
     }
 });
-client.once(Events.ClientReady, (c) => {
+
+client.once(Events.ClientReady, c => {
     console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
